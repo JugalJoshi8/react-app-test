677073e02f2000a5ef2f0af1713e5a74a4d77beb
Jugal Joshi <jkj.619@gmail.com>
2018-10-30 19:54:31 +0530

    person list changes

--- src/App.js
+++ src/App.js
@@ -4,31 +4,51 @@ import Person from './Person/Person';
 
 class App extends Component {
   state = {
-    persons: [{ name: 'Jugal', age: 30 },
-    { name: 'Temp', age: 30 },
-    { name: 'Temp2', age: 30 }]
+    persons: [{ id: 12, name: 'Jugal', age: 30 },
+    { id: 13, name: 'Temp', age: 30 },
+    { id: 14, name: 'Temp2', age: 30 }],
+    showPersons: true
   };
 
-  changeState = () => {
+  changeName = (event, id) => {
+    const newName = event.target.value;
+    const personIndex = this.state.persons.findIndex((person) => person.id === id);
+    let person = this.state.persons[personIndex];
+    const newPerson = {...person, name: newName};
+    const newPersons = [...this.state.persons];
+    newPersons[personIndex] = newPerson;
     this.setState({
-      persons: [{ name: `JKJ${Math.floor(Math.random() * 30)}`, age: 30 },
-      { name: 'Temp', age: 30 },
-      { name: 'Temp2', age: 30 }]
-    })
+      persons: newPersons
+    });
+  }
+
+  deletePerson(id) {
+    const personIndex = this.state.persons.findIndex((person) => person.id === id);
+    const newPersons = [...this.state.persons];
+    newPersons.splice(personIndex, 1);
+    this.setState({
+      persons: newPersons
+    });
   }
 
   render() {
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = this.state.persons.map((person) => {
+        return <Person deletePerson = {() => this.deletePerson(person.id)} key= {person.id} changeName={(event) => this.changeName(event, person.id)} name={person.name} age={person.age} >
+        </Person>
+      })
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           Persons
         </header>
+        <button onClick={() => { this.setState({ showPersons: !this.state.showPersons }) }}>Show / Hide Persons</button>
         <ul>
-          <Person changeName = {this.changeState} name={this.state.persons[0].name} age={this.state.persons[0].age} ></Person>
-          <Person changeName = {this.changeState} name={this.state.persons[1].name} age={this.state.persons[1].age} ></Person>
-          <Person changeName = {this.changeName} name={this.state.persons[2].name} age={this.state.persons[2].age} ></Person>
+          {persons}
         </ul>
-
         <button onClick={this.changeState}>Change Name</button>
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,8 +4,13 @@ import './Person.css';
  const person = (props) => {
     return (
         <div className = 'person'>
-            <p onClick = {props.changeName} >Name : {props.name}</p>
+            <p>Name : {props.name}</p>
             <p>Age: {props.age}</p>
+            <div>
+                <label>New Name: </label>
+                <input value = {props.name} onChange = {props.changeName}></input>
+            </div>
+            <button onClick = {props.deletePerson}>Delete</button>
         </div>
     )
 }
